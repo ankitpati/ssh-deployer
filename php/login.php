a48c440238905209387dcfeee8dd88fee3c7a4b6
Ankit Pati <ankitpati@gmail.com>
2017-01-11 11:48:37 +0530

    password hashing and verification logic added

--- php/login.php
+++ php/login.php
@@ -4,10 +4,37 @@
 
     $con = new PDO("mysql:host=localhost;dbname=tasktracker", "tasktracker", "tasktracker");
 
-    $ps = $con->prepare("select 1 from users where username = ?");
-    $ps->execute(array($_REQUEST["username"]));
+    if (!isset($_REQUEST["password"])) {
+        $ps = $con->prepare("select 1 from users where username = ?");
+        $ps->execute(array($_REQUEST["username"]));
 
-    echo $ps->fetchColumn() ? 1 : 0;
+        echo $ps->fetchColumn() ? 1 : 0;
+    }
+
+    if (isset($_REQUEST["login"])) {
+        if ($_REQUEST["login"] === "true") {
+            $ps = $con->prepare("select password from users where username = ?");
+            $ps->execute(array($_REQUEST["username"]));
+
+            if (password_verify($_REQUEST["password"], $ps->fetchColumn())) {
+                setcookie("username", $_REQUEST["username"], time() + 60*60*24*30, "/");
+                setcookie("password", $_REQUEST["password"], time() + 60*60*24*30, "/");
+                header("Location: ../index.html?status=correct");
+            }
+            else {
+                setcookie("username", "", 1, "/");
+                setcookie("password", "", 1, "/");
+                header("Location: ../index.html?status=incorrect");
+            }
+        }
+        else {
+            $ps = $con->prepare("insert into users values (?, ?)");
+            header(
+                "Location: ../index.html?status=".
+                ($ps->execute(array($_REQUEST["username"], password_hash($_REQUEST["password"], PASSWORD_DEFAULT))) ? "created" : "exists")
+            );
+        }
+    }
 
     $ps = null;
     $con = null;
