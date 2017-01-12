ba7067d9f198a7c09cad32f45ef473e61d3c25c8
Ankit Pati <ankitpati@gmail.com>
2017-01-12 09:11:21 +0530

    allow replacing previously uploaded sites with new uploads

--- php/workspace.php
+++ php/workspace.php
@@ -9,22 +9,24 @@
     $ps = $con->prepare("select password from users where username = ?");
     $ps->execute(array($_COOKIE["username"]));
 
+    function delete_dir($path) {
+        return is_file($path) ?
+                @unlink($path) :
+                array_map(__FUNCTION__, glob($path.'/*')) == @rmdir($path);
+    }
+
     if (password_verify($_COOKIE["password"], $ps->fetchColumn())) {
         $dirpath = "../apps/".$_COOKIE["username"]."/".pathinfo($_FILES["upload"]["name"])["filename"];
-        if (file_exists($dirpath)) {
-
+        if (file_exists($dirpath)) delete_dir($dirpath);
+        mkdir($dirpath, 0777, true);
+        $file = new ZipArchive;
+        if ($file->open($_FILES["upload"]["tmp_name"]) === true) {
+            $file->extractTo($dirpath."/");
+            $file->close();
+            header("Location: ../workspace.html?status=pass");
         }
-        else {
-            mkdir($dirpath, 0777, true);
-            $file = new ZipArchive;
-            if ($file->open($_FILES["upload"]["tmp_name"]) === true) {
-                $file->extractTo($dirpath."/");
-                $file->close();
-                header("Location: ../workspace.html?status=pass");
-            }
-            else
-                header("Location: ../workspace.html?status=fail");
-        }
+        else
+            header("Location: ../workspace.html?status=fail");
     }
     else {
         setcookie("username", "", 1, "/");
